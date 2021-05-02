3fcb90edf4793854ddc268148055ef3436e9dbec
Matts-R <matheushrdasilva13@gmail.com>
2021-05-02 12:00:34 -0300

    Tela de login e cadastro criadas

---
+++ frontend/src/Components/Container.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../Styles/container.css";
+
+export default function Containerx(props) {
+  return <div className="container">{props.children}</div>;
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Cadastro from "./Components/Cadastro";
+import Container from "./Components/Container";
+import Login from "./Components/Login";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <Router>
+      <Container>
+        <Switch>
+          <Route strict exact path="/">
+            <Login></Login>
+          </Route>
+          <Route strict exact path="/cadastro">
+            <Cadastro></Cadastro>
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
