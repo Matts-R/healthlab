da46ad911c1e4d9e2971325444e4fbee1e9b1fbf
David-Jacobis <david.jacobis@outlook.com>
2021-05-03 18:16:23 -0300

    adicionando funções de comparar senha

--- backend/services/Authenticate.js
+++ backend/services/Authenticate.js
@@ -12,6 +12,36 @@ class AuthenticationService {
 
   // TO-DO
   static async comparePassword(password) {
+    bcrypt.compare(password, hash, function(err, result) {
+// result == true
+if (this.password == this.hash){
+  try{
+  return result
+  }
+  catch (error){
+    return json({ error: err.mesage });
+  }
+}
+else{
+  return json({ Mensagem: "Não foi encontrado senha para comparar" });
+}
+  });
+
+  bcrypt.compare(password, hash, function(err, result) {
+    // result == false
+    if (this.password != this.hash){
+    try{
+      return result
+      }
+      catch (error){
+        return json({ error: err.mesage });
+      }
+    } else{
+      return json({ Mensagem: "Não foi encontrado senha para comparar" });
+    }
+});
+
+
     // const salt = await bcrypt.genSalt(this.rounds);
     // const hash = await bcrypt.hash(password, salt);
     // return hash;
