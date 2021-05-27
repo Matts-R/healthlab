f4abcfcb0fa40b2b2e05bc941fefa7e6813117e3
David-Jacobis <david.jacobis@outlook.com>
2021-05-26 22:28:54 -0300

    Adicionando tela post favoritos

--- frontend/src/app/components/favoritos/favoritos.component.ts
+++ frontend/src/app/components/favoritos/favoritos.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favoritos.component.css']
 })
 export class FavoritosComponent implements OnInit {
-
+  public posts: Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.posts = [
+      {
+        name: 'Bla',
+        quando: 'bli',
+        por: 'kk',
+      },
+      {
+        name: 'Bla',
+        quando: 'bli',
+        por: 'kk',
+      },
+      {
+        name: 'Bla',
+        quando: 'bli',
+        por: 'kk',
+      },
+      {
+        name: 'Bla',
+        quando: 'bli',
+        por: 'kk',
+      },
+    ];
 
+    console.log(this.posts);
+  }
+  teste():void{
+    alert("Removido dos Favoritos")  
+  } 
 }
