abab8ea26d4cc4e536883bcc9a7289531f752b49
David-Jacobis <david.jacobis@outlook.com>
2021-05-28 15:23:57 -0300

    Corrigindo erros

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,31 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-<<<<<<< HEAD
 import { TelaLoginComponent } from './components/tela-login/tela-login.component';
 import { TelaCadastroComponent } from './components/tela-cadastro/tela-cadastro.component';
-=======
 import { FavoritosComponent } from './components/favoritos/favoritos.component';
-<<<<<<< HEAD
->>>>>>> 132fe27da22e1a9a5f47259225cb9a9f4876d244
-=======
 import { SeguindoComponent } from './components/seguindo/seguindo.component';
->>>>>>> c3fdb10ef249cf4a981b27489fc8b014316363a6
+
 
 const routes: Routes = [
   { path: '', component: NavBarComponent },
   { path: 'home', component: HomeComponent },
-<<<<<<< HEAD
-<<<<<<< HEAD
   { path: 'login', component: TelaLoginComponent },
-  { path: 'cadastro', component: TelaCadastroComponent }
-=======
-  { path: 'favoritos', component: FavoritosComponent}
->>>>>>> 132fe27da22e1a9a5f47259225cb9a9f4876d244
-=======
+  { path: 'cadastro', component: TelaCadastroComponent },
   { path: 'favoritos', component: FavoritosComponent},
   { path: 'seguindo', component: SeguindoComponent}
->>>>>>> c3fdb10ef249cf4a981b27489fc8b014316363a6
+
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -10,19 +9,12 @@ import { PostCardComponent } from './components/post-card/post-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatChipsModule } from '@angular/material/chips';
 import { TagsCardComponent } from './components/tags-card/tags-card.component';
-<<<<<<< HEAD
 import { TelaLoginComponent } from './components/tela-login/tela-login.component';
 import { TelaCadastroComponent } from './components/tela-cadastro/tela-cadastro.component';
-=======
 import { FavoritosComponent } from './components/favoritos/favoritos.component';
-<<<<<<< HEAD
->>>>>>> 132fe27da22e1a9a5f47259225cb9a9f4876d244
-
-
-=======
 import { HttpClientModule } from '@angular/common/http';
 import { SeguindoComponent } from './components/seguindo/seguindo.component';
->>>>>>> c3fdb10ef249cf4a981b27489fc8b014316363a6
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,17 +23,12 @@ import { SeguindoComponent } from './components/seguindo/seguindo.component';
     UserCardComponent,
     PostCardComponent,
     TagsCardComponent,
-<<<<<<< HEAD
     TelaLoginComponent,
     TelaCadastroComponent,
-=======
     FavoritosComponent,
-<<<<<<< HEAD
 
->>>>>>> 132fe27da22e1a9a5f47259225cb9a9f4876d244
-=======
     SeguindoComponent,
->>>>>>> c3fdb10ef249cf4a981b27489fc8b014316363a6
+
   ],
   imports: [
     BrowserModule,
