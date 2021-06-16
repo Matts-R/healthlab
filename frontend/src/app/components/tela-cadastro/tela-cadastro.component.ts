44b2260142829b5ad85c727efdf6f8c6aa5e4691
David-Jacobis <david.jacobis@outlook.com>
2021-06-16 13:44:18 -0300

    Modificações Finais

--- frontend/src/app/components/tela-cadastro/tela-cadastro.component.ts
+++ frontend/src/app/components/tela-cadastro/tela-cadastro.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tela-cadastro',
   templateUrl: './tela-cadastro.component.html',
   styleUrls: ['./tela-cadastro.component.css']
 })
 export class TelaCadastroComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    toLogin(){
+      this.router.navigate(['tela-login']);
+    }
 }
 
 
